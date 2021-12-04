1e3b5f74e2e7891d67448b1d129d26ec49389fcb
Naveen Satti <naveen.satti@dxc.com>
2021-12-04 15:53:35 +0530

    added API's

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/bmi', (req, res) => {
+    try {
+        let BMIdata = formatData(data)
+        res.status(200).json({ BMIdata })
+    } catch (err) {
+        res.status(400).json({ 'message': `unable to process`, 'error': JSON.stringify(err) })
+    }
+})
+
+app.get('/bmi/:category/count', (req, res) => {
+    try {
+        let count = getCountByCategory(data, req.params.category)
+        res.status(200).json({ count,  category : req.params.category})
+    } catch (err) {
+        res.status(400).json({ 'message': `unable to caclulate count`, 'error': JSON.stringify(err) })
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
 let data = [{ "Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, {
@@ -19,51 +33,52 @@ let data = [{ "Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, {
 }, { "Gender": "Female", "HeightCm": 150, "WeightKg": 70 },
 { "Gender": "Female", "HeightCm": 167, "WeightKg": 82 }]
 
-data.forEach(_ => {
-    let BMI = calculateBMI(_)
-    _.BMI = BMI.toFixed(2)
-    _.BMICategory = getBMICategory(BMI)
-    _.HealthRisk = getHealthRisk(BMI)
-})
-console.log(data)
-let overWeightCount = getCountByCategory(data, `Overweight`)
-console.log(overWeightCount)
-function calculateBMI (weightObj) {
+function formatData(data) {
+    data.forEach(_ => {
+        let BMI = calculateBMI(_)
+        _.BMI = BMI.toFixed(2)
+        _.BMICategory = getBMICategory(BMI)
+        _.HealthRisk = getHealthRisk(BMI)
+    })
+    return data
+}
+
+function calculateBMI(weightObj) {
     return (weightObj.WeightKg * 100) / weightObj.HeightCm
 }
 
-function getBMICategory(BMI){
-    if(BMI <= 18.4){
+function getBMICategory(BMI) {
+    if (BMI <= 18.4) {
         return `Underweight`
-    }else if(BMI >= 18.5 && BMI <=24.9){
+    } else if (BMI >= 18.5 && BMI <= 24.9) {
         return `Normal weight`
-    }else if(BMI >= 25 && BMI <=29.9){
+    } else if (BMI >= 25 && BMI <= 29.9) {
         return `Overweight`
-    }else if(BMI >= 30 && BMI <=34.9){
+    } else if (BMI >= 30 && BMI <= 34.9) {
         return `Moderately obese`
-    }else if(BMI >= 35 && BMI <=39.9){
+    } else if (BMI >= 35 && BMI <= 39.9) {
         return `Severel obese`
-    }else if(BMI >= 40){
+    } else if (BMI >= 40) {
         return `Very severely obese`
     }
 }
 
-function getHealthRisk(BMI){
-    if(BMI <= 18.4){
+function getHealthRisk(BMI) {
+    if (BMI <= 18.4) {
         return `Malnutrition risk`
-    }else if(BMI >= 18.5 && BMI <=24.9){
+    } else if (BMI >= 18.5 && BMI <= 24.9) {
         return `Low risk`
-    }else if(BMI >= 25 && BMI <=29.9){
+    } else if (BMI >= 25 && BMI <= 29.9) {
         return `Enhanced risk`
-    }else if(BMI >= 30 && BMI <=34.9){
+    } else if (BMI >= 30 && BMI <= 34.9) {
         return `Medium risk`
-    }else if(BMI >= 35 && BMI <=39.9){
+    } else if (BMI >= 35 && BMI <= 39.9) {
         return `High`
-    }else if(BMI >= 40){
+    } else if (BMI >= 40) {
         return `Very high risk`
     }
 }
 
-function getCountByCategory(data, category){
+function getCountByCategory(data, category) {
     return data.filter(_ => _.BMICategory == category).length
 }
